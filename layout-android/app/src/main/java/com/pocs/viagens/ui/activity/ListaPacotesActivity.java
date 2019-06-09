6402d89e109fa6614529eeeea212dd5e6cb02fed
luizantoniojr <luizantoniosilvajr@outlook.com>
2019-06-09 14:10:30 -0300

    layout-android-1 aula 1 atividade 1-9

---
+++ layout-android/app/src/main/java/com/pocs/viagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,16 @@
+package com.pocs.viagens.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.pocs.viagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
